8c3f3ec28578e32a73db94f93e37465a0ee2daff
HemanthAchu <hemanthms186@gmail.com>
2024-02-04 11:43:45 +0530

    complete Sidebar uI

--- blog client/src/pages/Dashboard.jsx
+++ blog client/src/pages/Dashboard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import { useLocation } from "react-router-dom"
+import DashSidebar from "../Components/DashSidebar"
+import DashProfile from "../Components/DashProfile"
+
 
 export default function Dashboard() {
+  const location = useLocation()
+  const [tab, settab] = useState('')
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabFromUrl = urlParams.get('tab')
+    if (tabFromUrl) {
+      settab(tabFromUrl)
+    }
+
+  }, [location.search])
   return (
-    <div>
-      Dashboard
+    <div className="min-h-screen flex flex-col  md:flex-row ">
+
+      <div className="md:w-56">
+        <DashSidebar />
+      </div>
+      {tab === 'profile' && <DashProfile />}
+
     </div>
   )
 }
